2f99c54b21ae3394e828d3fdbd9772225b590d42
Aldi Alimucaj <aldi@outlook.com>
2013-11-16 21:44:58 +0100

    [NT] ClientListActivity
    * edit and synchronize

--- src/al/aldi/tope/ClientListActivityTest.java
+++ src/al/aldi/tope/ClientListActivityTest.java
@@ -1,10 +1,12 @@
 package al.aldi.tope;
 
 import al.aldi.libjaldi.string.AldiStringUtils;
+import al.aldi.tope.utils.TopeUtils;
 import al.aldi.tope.view.activities.ClientsListActivity;
 import android.app.Activity;
 import android.app.ListActivity;
 import android.test.ActivityInstrumentationTestCase2;
+import android.widget.ListView;
 import com.jayway.android.robotium.solo.Solo;
 
 /**
@@ -15,8 +17,9 @@ import com.jayway.android.robotium.solo.Solo;
  * [Add Description]
  */
 public class ClientListActivityTest extends ActivityInstrumentationTestCase2<ClientsListActivity> {
-    private Solo     solo              = null;
-    private Activity activityUnderTest = null;
+    public static final String   TEST_SERVER       = "192.168.178.50";
+    private             Solo     solo              = null;
+    private             Activity activityUnderTest = null;
 
     public ClientListActivityTest() {
         super(ClientsListActivity.class);
@@ -31,8 +34,7 @@ public class ClientListActivityTest extends ActivityInstrumentationTestCase2<Cli
 
     @Override
     public void tearDown() throws Exception {
-        super.tearDown();
-//        solo.finishOpenedActivities();
+        solo.finishOpenedActivities();
     }
 
     public void testAddNewServer() throws Exception {
@@ -40,31 +42,127 @@ public class ClientListActivityTest extends ActivityInstrumentationTestCase2<Cli
         solo.assertCurrentActivity("Expected ClientListActivity activity", activityName);
         solo.sleep(2000);
         int initialCount = ((ListActivity) activityUnderTest).getListView().getAdapter().getCount();
+        if (0 == initialCount) {
+            solo.waitForText(activityUnderTest.getString(R.string.client_list_empty), 1, 1000);
+            solo.clickOnButton(activityUnderTest.getString(R.string.no));
+        }
 
         // opening add and edit activity
         solo.clickOnMenuItem(activityUnderTest.getString(R.string.action_clients_add));
+
         String serverName = AldiStringUtils.random(5);
         solo.enterText(0, serverName);
-        solo.enterText(1, "192.168.178.50");
+        solo.enterText(1, TEST_SERVER);
         solo.clickOnButton(activityUnderTest.getString(R.string.title_button_ok));
-        assert (solo.waitForActivity(activityUnderTest.getClass(), 1000));
+        assertTrue(solo.waitForActivity(activityUnderTest.getClass(), 1000));
         // waiting for response to come
-        assert (solo.waitForText(activityUnderTest.getString(R.string.task_executed_sccuessfully), 1, 10000));
+        solo.waitForText("(?i).*?successfully", 1, 10000);
         // waiting for response to be processed
         solo.sleep(3000);
 
         // it should appear in the list
-        int secondCount = ((ListActivity) activityUnderTest).getListView().getAdapter().getCount();
-        assert (secondCount == initialCount + 1);
+        ListView lv = ((ListActivity) activityUnderTest).getListView();
+        int secondCount = lv.getCount();
+        //assertTrue(secondCount == initialCount + 1);
 
         // checking if new server appears in list
-        assert (solo.searchText(serverName, true));
+        assertTrue(solo.searchText(serverName, true));
         // cleaning up the added server
         solo.clickLongOnText(serverName);
         solo.clickOnText(activityUnderTest.getString(R.string.client_edit_delete));
         solo.clickOnText(activityUnderTest.getString(R.string.yes));
         solo.sleep(2000);
         int thirdCount = ((ListActivity) activityUnderTest).getListView().getAdapter().getCount();
-        assert (initialCount == thirdCount);
+        assertTrue(initialCount == thirdCount);
+    }
+
+    /**
+     * Testing synchronize Client. It requires a server to be available
+     *
+     * @throws Exception
+     */
+    public void testSynchronizeServer() throws Exception {
+        boolean newItemAdded = false;
+        String activityName = activityUnderTest.getLocalClassName().substring(activityUnderTest.getLocalClassName().lastIndexOf(".") + 1);
+        solo.assertCurrentActivity("Expected ClientListActivity activity", activityName);
+        solo.sleep(2000);
+        // there should be at least one item
+        int listCount = ((ListActivity) activityUnderTest).getListView().getAdapter().getCount();
+        // if not add one to synchronize
+        if (listCount < 1) {
+            solo.waitForText(activityUnderTest.getString(R.string.client_list_empty), 1, 1000);
+            solo.clickOnButton(activityUnderTest.getString(R.string.no));
+            // opening add and edit activity
+            solo.clickOnMenuItem(activityUnderTest.getString(R.string.action_clients_add));
+            String serverName = AldiStringUtils.random(5);
+            solo.enterText(0, serverName);
+            solo.enterText(1, TEST_SERVER);
+            solo.clickOnButton(activityUnderTest.getString(R.string.title_button_ok));
+            assertTrue(solo.waitForActivity(activityUnderTest.getClass(), 1000));
+            // waiting for response to come
+            solo.waitForText(activityUnderTest.getString(R.string.task_executed_sccuessfully), 1, 10000);
+            // waiting for response to be processed
+            solo.sleep(3000);
+            newItemAdded = true;
+        }
+        int secondCount = ((ListActivity) activityUnderTest).getListView().getAdapter().getCount();
+        //assertTrue(secondCount == 1);
+        solo.clickLongInList(1);
+        solo.clickOnText(activityUnderTest.getString(R.string.client_edit_synchronize));
+        solo.waitForText(activityUnderTest.getString(R.string.client_edit_synchronize_client), 1, 1000);
+        solo.waitForText(activityUnderTest.getString(R.string.task_executed_sccuessfully), 1, 10000);
+        solo.sleep(2000);
+
+        if (newItemAdded) {
+            solo.clickLongInList(1);
+            solo.clickOnText(activityUnderTest.getString(R.string.client_edit_delete));
+            solo.clickOnText(activityUnderTest.getString(R.string.yes));
+            solo.sleep(2000);
+        }
+        int thirdCount = ((ListActivity) activityUnderTest).getListView().getAdapter().getCount();
+        assertTrue(listCount == thirdCount);
+    }
+
+    public void testEditServer() throws Exception {
+        boolean newItemAdded = false;
+        String activityName = activityUnderTest.getLocalClassName().substring(activityUnderTest.getLocalClassName().lastIndexOf(".") + 1);
+        solo.assertCurrentActivity("Expected ClientListActivity activity", activityName);
+        solo.sleep(2000);
+        // there should be at least one item
+        int listCount = ((ListActivity) activityUnderTest).getListView().getAdapter().getCount();
+        // click no if list is empty
+        if (listCount == 0) {
+            solo.waitForText(activityUnderTest.getString(R.string.client_list_empty), 1, 1000);
+            solo.clickOnButton(activityUnderTest.getString(R.string.no));
+        }
+        // opening add and edit activity
+        solo.clickOnMenuItem(activityUnderTest.getString(R.string.action_clients_add));
+        String serverName = AldiStringUtils.random(5);
+        solo.enterText(0, serverName);
+        solo.enterText(1, TEST_SERVER);
+        solo.clickOnButton(activityUnderTest.getString(R.string.title_button_ok));
+        assertTrue(solo.waitForActivity(activityUnderTest.getClass(), 1000));
+        // waiting for response to come
+        solo.waitForText(activityUnderTest.getString(R.string.task_executed_sccuessfully), 1, 10000);
+        // waiting for response to be processed
+        solo.sleep(3000);
+        newItemAdded = true;
+
+        int secondCount = ((ListActivity) activityUnderTest).getListView().getAdapter().getCount();
+        //assertTrue(secondCount == listCount + 1);
+        solo.clickLongOnText(serverName);
+        solo.clickOnText(activityUnderTest.getString(R.string.client_edit_edit));
+        assertTrue(solo.searchText(serverName));
+        assertTrue(solo.searchText(TEST_SERVER));
+        assertTrue(solo.searchText(TopeUtils.TOPE_DEFAULT_PORT));
+        solo.clickOnButton(activityUnderTest.getString(R.string.title_button_cancel));
+
+        solo.clickLongOnText(serverName);
+        solo.clickOnText(activityUnderTest.getString(R.string.client_edit_delete));
+        solo.clickOnText(activityUnderTest.getString(R.string.yes));
+        solo.sleep(2000);
+
+        int thirdCount = ((ListActivity) activityUnderTest).getListView().getAdapter().getCount();
+        assertTrue(listCount == thirdCount);
     }
 }
